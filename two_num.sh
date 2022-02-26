708ef6f37edeec5c98cb1bf0a09dc8c62a6902c9
Darshil Borole <darshilborole20@gmail.com>
2022-02-26 18:46:00 +0530

    Addition of 2 numbers

---
+++ two_num.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Calculate the sum of two integers with pre initialize values
+# in a shell script
+ 
+a=40
+b=20
+ 
+sum=$(( $a + $b )) 
+minus=$(( $a - $b ))
+echo "sum of $a and $b is ==>  $sum"
+echo "minus of $a and $b is ==>  $minus"
+
